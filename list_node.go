595965a487016f046b013dbe99b69b9eb08889bd
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-10-25 07:28:34 -0700

    a simple list node struct to serve as the basic building block

---
+++ list_node.go
@@ -0,0 +1,12 @@
+package gods
+
+// ListNode represents one node in a linked list
+type ListNode struct {
+	Data interface{}
+	Next *ListNode
+}
+
+// NewListNode will return a ListNode that contains the passed in data
+func NewListNode(data interface{}) *ListNode {
+	return &ListNode{data, nil}
+}
