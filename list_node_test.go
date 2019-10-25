bdee9a33cb1a2485fe668b936991da3f706a8232
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-10-25 07:28:49 -0700

    a simple test to make sure that the constructor works

---
+++ list_node_test.go
@@ -0,0 +1,28 @@
+package gods_test
+
+import (
+	"testing"
+
+	"github.com/YashdalfTheGray/gods"
+)
+
+func TestNew(t *testing.T) {
+	testCases := []struct {
+		desc string
+		in   interface{}
+		out  *gods.ListNode
+	}{
+		{
+			desc: "creates a new list node with the data",
+			in:   5,
+			out:  &gods.ListNode{Data: 5, Next: nil},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			if ln := gods.NewListNode(tC.in); ln.Data != tC.out.Data {
+				t.Errorf("Expected %s but got %s", tC.in, tC.out.Data)
+			}
+		})
+	}
+}
