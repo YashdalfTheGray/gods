d767a8c5dbbc54b16017f659c36e886b7e32c35d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-10-30 01:08:17 -0700

    rearraged tests and added tests for push

--- singly_linked_list_test.go
+++ singly_linked_list_test.go
@@ -33,7 +33,7 @@ func TestSinglyLinkedListShift(t *testing.T) {
 		in3     string
 		addAll  bool
 		prepend []string
-		legnth  uint32
+		length  uint32
 	}{
 		{
 			desc:    "can put something in an empty list",
@@ -42,7 +42,7 @@ func TestSinglyLinkedListShift(t *testing.T) {
 			in3:     "",
 			addAll:  false,
 			prepend: []string{},
-			legnth:  1,
+			length:  1,
 		},
 		{
 			desc:    "can put something in a non-empty list",
@@ -51,7 +51,7 @@ func TestSinglyLinkedListShift(t *testing.T) {
 			in3:     "",
 			addAll:  false,
 			prepend: []string{"foo"},
-			legnth:  2,
+			length:  2,
 		},
 		{
 			desc:    "supports fluent API",
@@ -60,7 +60,7 @@ func TestSinglyLinkedListShift(t *testing.T) {
 			in3:     "other things",
 			addAll:  true,
 			prepend: []string{},
-			legnth:  3,
+			length:  3,
 		},
 	}
 	for _, tC := range testCases {
@@ -77,49 +77,8 @@ func TestSinglyLinkedListShift(t *testing.T) {
 				ll.Shift(tC.in1)
 			}
 
-			if ll.Length() != tC.legnth {
-				t.Errorf("Expected length to be 1 but got %d", ll.Length())
-			}
-		})
-	}
-}
-
-func TestSinglyLinkedListIterator(t *testing.T) {
-	testCases := []struct {
-		desc    string
-		content []int
-	}{
-		{
-			desc:    "iterates over a linked list with some stuff",
-			content: []int{1, 2, 3, 4},
-		},
-		{
-			desc:    "iterates over a linked list with one stuff",
-			content: []int{5},
-		},
-		{
-			desc:    "iterates over a linked list with no stuff",
-			content: []int{},
-		},
-	}
-	for _, tC := range testCases {
-		t.Run(tC.desc, func(t *testing.T) {
-			ll := gods.NewSinglyLinkedList()
-
-			for i := len(tC.content) - 1; i >= 0; i-- {
-				ll.Shift(tC.content[i])
-			}
-
-			iterator := ll.Iterate()
-			for i := 0; iterator.Next(); {
-				if v := iterator.Get(); v != tC.content[i] {
-					t.Errorf("Expected %d at position %d but got %d", tC.content[i], i, v)
-				}
-				i++
-			}
-
-			if err := iterator.Error(); err != nil {
-				t.Errorf("Expected the iterator to not be in an error state")
+			if ll.Length() != tC.length {
+				t.Errorf("Expected length to be %d but got %d", tC.length, ll.Length())
 			}
 		})
 	}
@@ -166,6 +125,106 @@ func TestSinglyLinkedListUnshift(t *testing.T) {
 
 			if result != tC.result {
 				t.Errorf("Expected unshift to return %d but got %d", tC.result, result)
+			}
+		})
+	}
+}
+
+func TestSinglyLinkedListPush(t *testing.T) {
+	testCases := []struct {
+		desc    string
+		in1     string
+		in2     string
+		in3     string
+		addAll  bool
+		prepend []string
+		length  uint32
+	}{
+		{
+			desc:    "can put something in an empty list",
+			in1:     "things",
+			in2:     "",
+			in3:     "",
+			addAll:  false,
+			prepend: []string{},
+			length:  1,
+		},
+		{
+			desc:    "can put something in a non-empty list",
+			in1:     "bar",
+			in2:     "",
+			in3:     "",
+			addAll:  false,
+			prepend: []string{"foo"},
+			length:  2,
+		},
+		{
+			desc:    "supports fluent API",
+			in1:     "things",
+			in2:     "stuff",
+			in3:     "other things",
+			addAll:  true,
+			prepend: []string{},
+			length:  3,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			ll := gods.NewSinglyLinkedList()
+
+			for _, v := range tC.prepend {
+				ll.Push(v)
+			}
+
+			if tC.addAll {
+				ll.Push(tC.in1).Push(tC.in2).Push(tC.in3)
+			} else {
+				ll.Push(tC.in1)
+			}
+
+			if ll.Length() != tC.length {
+				t.Errorf("Expected length to be %d but got %d", tC.length, ll.Length())
+			}
+		})
+	}
+}
+
+func TestSinglyLinkedListIterator(t *testing.T) {
+	testCases := []struct {
+		desc    string
+		content []int
+	}{
+		{
+			desc:    "iterates over a linked list with some stuff",
+			content: []int{1, 2, 3, 4},
+		},
+		{
+			desc:    "iterates over a linked list with one stuff",
+			content: []int{5},
+		},
+		{
+			desc:    "iterates over a linked list with no stuff",
+			content: []int{},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			ll := gods.NewSinglyLinkedList()
+
+			for i := len(tC.content) - 1; i >= 0; i-- {
+				ll.Shift(tC.content[i])
+			}
+
+			iterator := ll.Iterate()
+			for i := 0; iterator.Next(); {
+				if v := iterator.Get(); v != tC.content[i] {
+					t.Errorf("Expected %d at position %d but got %d", tC.content[i], i, v)
+				}
+				i++
+			}
+
+			if err := iterator.Error(); err != nil {
+				t.Errorf("Expected the iterator to not be in an error state")
 			}
 		})
 	}
