de5b9c2a410d37a0242c5f4f6f846cceebffe43f
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-11-03 09:33:40 -0800

    created a doubly linked list file

---
+++ doubly_linked_list.go
@@ -0,0 +1,18 @@
+package gods
+
+// DoublyLinkedList is a type of list with nodes
+// that hold references to the node before and the
+// node after.
+type DoublyLinkedList struct {
+	head     *ListNode
+	tail     *ListNode
+	length   uint32
+	circular bool
+}
+
+// NewDoublyLinkedList created a new list and returns it.
+// Passing in true to the constructor will make it a circular
+// list, i.e., the last node will point back to the first node
+func NewDoublyLinkedList(circular bool) DoublyLinkedList {
+	return DoublyLinkedList{circular: circular}
+}
