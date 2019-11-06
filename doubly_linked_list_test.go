a832cafdb4f00f288bb6f2df8db5f2b7cae2e750
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-11-06 03:30:21 -0800

    added some tests for push for doubly linked list

--- doubly_linked_list_test.go
+++ doubly_linked_list_test.go
@@ -213,3 +213,86 @@ func TestDoublyLinkedListUnshift(t *testing.T) {
 		})
 	}
 }
+
+func TestDoublyLinkedListPush(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		in1      string
+		in2      string
+		in3      string
+		addAll   bool
+		prepend  []string
+		circular bool
+		length   uint32
+	}{
+		{
+			desc:     "can put something in an empty list",
+			in1:      "things",
+			in2:      "",
+			in3:      "",
+			addAll:   false,
+			prepend:  []string{},
+			circular: false,
+			length:   1,
+		},
+		{
+			desc:     "can put something in a non-empty list",
+			in1:      "bar",
+			in2:      "",
+			in3:      "",
+			addAll:   false,
+			prepend:  []string{"foo"},
+			circular: false,
+			length:   2,
+		},
+		{
+			desc:     "supports fluent API",
+			in1:      "things",
+			in2:      "stuff",
+			in3:      "other things",
+			addAll:   true,
+			prepend:  []string{},
+			circular: false,
+			length:   3,
+		},
+		{
+			desc:     "can put something in an empty circular list",
+			in1:      "things",
+			in2:      "",
+			in3:      "",
+			addAll:   false,
+			prepend:  []string{},
+			circular: true,
+			length:   1,
+		},
+		{
+			desc:     "can put something in a non-empty circular list",
+			in1:      "bar",
+			in2:      "",
+			in3:      "",
+			addAll:   false,
+			prepend:  []string{"foo"},
+			circular: true,
+			length:   2,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			ll := gods.NewDoublyLinkedList(tC.circular)
+
+			for _, v := range tC.prepend {
+				ll.Push(v)
+			}
+
+			if tC.addAll {
+				ll.Push(tC.in1).Push(tC.in2).Push(tC.in3)
+			} else {
+				ll.Push(tC.in1)
+			}
+
+			if ll.Length() != tC.length {
+				t.Errorf("Expected length to be %d but got %d", tC.length, ll.Length())
+			}
+		})
+	}
+}
