c3a6424ed77ae8042bd59ea12b7cbe08f03ad219
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-11-23 16:18:14 -0800

    added tests for stack push

--- stack_test.go
+++ stack_test.go
@@ -24,3 +24,62 @@ func TestNewStack(t *testing.T) {
 		})
 	}
 }
+
+func TestStackPush(t *testing.T) {
+	testCases := []struct {
+		desc    string
+		in1     string
+		in2     string
+		in3     string
+		addAll  bool
+		prepend []string
+		length  uint32
+	}{
+		{
+			desc:    "can push something on an empty stack",
+			in1:     "things",
+			in2:     "",
+			in3:     "",
+			addAll:  false,
+			prepend: []string{},
+			length:  1,
+		},
+		{
+			desc:    "can push something on a non-empty stack",
+			in1:     "bar",
+			in2:     "",
+			in3:     "",
+			addAll:  false,
+			prepend: []string{"foo"},
+			length:  2,
+		},
+		{
+			desc:    "supports fluent API",
+			in1:     "things",
+			in2:     "stuff",
+			in3:     "other things",
+			addAll:  true,
+			prepend: []string{},
+			length:  3,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			s := gods.NewStack()
+
+			for _, v := range tC.prepend {
+				s.Push(v)
+			}
+
+			if tC.addAll {
+				s.Push(tC.in1).Push(tC.in2).Push(tC.in3)
+			} else {
+				s.Push(tC.in1)
+			}
+
+			if s.Length() != tC.length {
+				t.Errorf("Expected length to be %d but got %d", tC.length, s.Length())
+			}
+		})
+	}
+}
